d38048863c94da749dbf0d70e4205511a7a190d1
adititemkar <adititemkar2001.com>
2021-10-01 19:14:51 +0530

    javascript calculator

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+const a = document.querySelectorAll("input")[0];
+const b = document.querySelectorAll("input")[1];
+const add = document.getElementById("add");
+const multiply = document.getElementById("multiply");
+const subract = document.getElementById("subract");
+const result = document.querySelector(".result");
 
 
-const callbackfunc = (event) => {
-    const inputvalue = input.value
-    const element = document.createElement('li')
-    const textnode = document.createTextNode(inputvalue)
-    element.appendChild(textnode)
-    list.appendChild(element)
-}
-button.addEventListener('click', callbackfunc)
+
+// Calculate
+const calculate = (operation) => {
+    switch (operation) {
+        case "add":
+            result.innerHTML = parseInt(a.value) + parseInt(b.value);
+            break;
+        case "multiply":
+            result.innerHTML = parseInt(a.value) * parseInt(b.value);
+            break;
+        case "subract":
+            result.innerHTML = parseInt(a.value) - parseInt(b.value);
+            break;
+        default:
+            result.innerHTML = "Not a valid operation";
+    }
+};
+
+add.addEventListener("click", () => {
+    return calculate("add");
+});
+multiply.addEventListener("click", () => {
+    return calculate("multiply");
+});
+subract.addEventListener("click", () => {
+    return calculate("subract");
+})

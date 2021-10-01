6659ae232f7924325d11add44e4e8a8a890e4a79
adititemkar <adititemkar2001.com>
2021-10-01 20:05:48 +0530

    todo-list

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
-const a = document.querySelectorAll("input")[0];
-const b = document.querySelectorAll("input")[1];
-const add = document.getElementById("add");
-const multiply = document.getElementById("multiply");
-const subract = document.getElementById("subract");
-const result = document.querySelector(".result");
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
+
+var chores = []
+
+var deleteItem = (value) => {
+    const index = chores.indexOf(value)
+    console.log(index)
+}
 
 
-
-// Calculate
-const calculate = (operation) => {
-    switch (operation) {
-        case "add":
-            result.innerHTML = parseInt(a.value) + parseInt(b.value);
-            break;
-        case "multiply":
-            result.innerHTML = parseInt(a.value) * parseInt(b.value);
-            break;
-        case "subract":
-            result.innerHTML = parseInt(a.value) - parseInt(b.value);
-            break;
-        default:
-            result.innerHTML = "Not a valid operation";
+const callbackfunc = (event) => {
+    // console.log(input.value)
+    const inputValue = input.value
+    if (chores.includes(inputValue)) {
+        console.log('already exists')
+    } else {
+        chores.push(inputValue)
+        const element = document.createElement('li')
+        const textNode = document.createTextNode(inputValue)
+        element.appendChild(textNode)
+        list.appendChild(element)
+        element.addEventListener('click', (e) => { e.target.remove() })
     }
-};
+}
 
-add.addEventListener("click", () => {
-    return calculate("add");
-});
-multiply.addEventListener("click", () => {
-    return calculate("multiply");
-});
-subract.addEventListener("click", () => {
-    return calculate("subract");
-})
+
+button.addEventListener('click', callbackfunc)

cf9cdde38e14a63750701d4c96a8f83444dc4ad0
adititemkar <adititemkar2001.com>
2021-10-14 19:50:04 +0530

    Node_route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3000
 
 app.use(express.json())
-
+const token = "TOP_SECRET"
 let products = [{ name: 'iPhone12 Case', price: '999' }, { name: 'iPhone13 Case', price: '1199' }, { name: 'iPhone13 Pro Case', price: '1499' }]
 
 // Middlewares
@@ -24,13 +24,22 @@ app.get('/products', (req, res) => {
 // -----------PRIVATE routes---------------
 
 app.post('/products/add', validator, (req, res) => {
-    const { name, price } = req.body
+        const { name, price } = req.body
 
-    products.push({
-        name,
-        price,
+        products.push({
+            name,
+            price,
+        })
+        res.send({ products })
     })
-    res.send({ products })
+    // -----------authentication
+app.post('/auth', (req, res) => {
+    const { email, password } = req.body
+    if (email === 'admin@mail.com' && password === 'password') {
+        res.send({ token })
+    } else {
+        res.send({ message: "UNAUTHORISED" })
+    }
 })
 
 app.listen(PORT, () => {

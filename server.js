1cce6d2d42af79cddebaef2ecadead76ea07538b
adititemkar <adititemkar2001.com>
2021-10-18 20:03:24 +0530

    categoryRoutes

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
 const express = require('express')
+    //const database = require('./database/db')
 const app = express()
-const PORT = 3000
+const categoryRoutes = require('./routes/categoryRoutes')
+
+console.log(categoryRoutes.stack)
 
 app.use(express.json())
-const token = "TOP_SECRET"
-let products = [{ name: 'iPhone12 Case', price: '999' }, { name: 'iPhone13 Case', price: '1199' }, { name: 'iPhone13 Pro Case', price: '1499' }]
 
-// Middlewares
-const validator = (req, res, next) => {
-    const { name, price } = req.body
+app.use('/', categoryRoutes)
 
-    if (!name || !price) res.json({ error: "Validation failed" })
-    else next()
-}
-
-
-
-
-// -----------PUBLIC routes---------------
-// GET ROUTE
-//  Send all products
-app.get('/products', (req, res) => {
-    res.json({ products })
-})
-
-// -----------PRIVATE routes---------------
-
-app.post('/products/add', validator, (req, res) => {
-        const { name, price } = req.body
-
-        products.push({
-            name,
-            price,
-        })
-        res.send({ products })
-    })
-    // -----------authentication
-app.post('/auth', (req, res) => {
-    const { email, password } = req.body
-    if (email === 'admin@mail.com' && password === 'password') {
-        res.send({ token })
-    } else {
-        res.send({ message: "UNAUTHORISED" })
+app.get('/', (req, res) => {
+    try {
+        console.log(document)
+    } catch (error) {
+        res.status(203).send(error.message)
     }
 })
-
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
+app.listen(3001, () => {
+    console.log("listning to port 3001")
 })

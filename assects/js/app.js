a688019ace42eee652afd2ef42fde9e621779ec2
adititemkar <adititemkar2001.com>
2021-10-20 19:20:18 +0530

    maps

---
+++ assects/js/app.js
@@ -0,0 +1,10 @@
+console.log(L)
+var map = L.map('map').setView([0, 0], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express')
-    //const database = require('./database/db')
 const app = express()
-const categoryRoutes = require('./routes/categoryRoutes')
+const category_Routes = require('./routes/categoryRoutes')
 
-console.log(categoryRoutes.stack)
-
+const productRoutes = require('./routes/productRoutes')
 app.use(express.json())
 
-app.use('/', categoryRoutes)
+app.use('/category', category_Routes)
+app.use('/products', productRoutes)
 
-app.get('/', (req, res) => {
-    try {
-        console.log(document)
-    } catch (error) {
-        res.status(203).send(error.message)
-    }
-})
 app.listen(3001, () => {
-    console.log("listning to port 3001")
+    console.log("Listening at PORT 3001")
 })
